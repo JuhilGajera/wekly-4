658f72b6a9525d95c7c01603ecf5f5e7402a2438
JuhilGajera <juhilgajera2@gmail.com>
2023-04-27 23:56:35 +0530

    updated

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const fetchTodos = async () => {
     const response = await fetch("https://fakestoreapi.com/products/14");
     const data = await response.json();
     console.log(data);
+    document.getElementById("img").innerHTML = `<img>${data.image}</img>`;
     document.getElementById("result").innerHTML = `<div>${data.title}</div>`;
     document.getElementById("category").innerHTML = `<h1>${data.category}</h1>`;
-    document.getElementById("img").innerHTML = `<img>${data.image}</img>`;
     
   } catch (error) {
     console.log(error);
